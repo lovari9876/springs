437a4c441472a61ea0aa9d7a602f4ab1ec77ece8
user <user@DESKTOP-V882PTR>
2020-01-23 12:05:26 +0900

    수정

--- day0123_spring_json/src/main/java/edu/bit/board/controller/BoardRestController.java
+++ day0123_spring_json/src/main/java/edu/bit/board/controller/BoardRestController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.bit.board.service.BoardService;
 import edu.bit.board.vo.BoardVO;
 
+// @Controller + @ResponseBody => 이제  @RestController로 바뀐것!!!
+// @RestController를 통해 모든 객체를 json으로 바꾸어준다!! (dependency 필수!!)
 
 @RestController
 @RequestMapping("/rest/*") // BoardController 와 차이를 두기 위해 매핑
@@ -18,6 +21,7 @@ public class BoardRestController {
 	@Autowired
 	private BoardService boardService;
 
+	@ResponseBody // 예전 버전이라고 해~~
 	@RequestMapping("/list")
 	public List<BoardVO> list(Model model) {
 		System.out.println("list()");
