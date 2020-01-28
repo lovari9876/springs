c56db6f71c86047633fa35d19ad0fc068bd6f039
user <user@DESKTOP-V882PTR>
2020-01-28 18:34:55 +0900

    cardinality 1:n관계에서 mybatis와 resultmap 이용

---
+++ day0128_spring_resultmap/src/main/java/edu/bit/board/service/BoardService2.java
@@ -0,0 +1,21 @@
+package edu.bit.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.bit.board.mapper.BoardMapper;
+import edu.bit.board.vo.BoardVO;
+
+@Service
+public class BoardService2 {
+	
+	@Autowired
+	BoardMapper boardMapper;
+	
+	public List<BoardVO> selectBoardList(){
+		return boardMapper.selectBoardList();
+	}
+	
+}
